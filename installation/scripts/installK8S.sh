d38be1b2db6a3ad8378feb8da59f6603072d796b
puneetgavri <puneet.gavri@gmail.com>
2024-01-24 04:36:22 +0530

    Update and rename installK8S-v1-23.sh to installK8S.sh

---
+++ installation/scripts/installK8S.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+install_ubuntu() {
+
+   #### Install Kubernetes latest components
+   sudo apt-get update
+   sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+   echo "starting the installation of k8s components (kubeadm,kubelet,kubectl) ...."
+   curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.26/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.25/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+   echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.24/deb/ /" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+   sudo apt-get update
+   sudo apt-get install -y kubelet kubeadm kubectl
+
+   if [ $? -eq 0 ];then
+      echo "kubelet, kubeadm & kubectl are successfully installed"
+      sudo apt-mark hold kubelet kubeadm kubectl
+   else
+      echo "issue in installing kubelet, kubeadm & kubectl - process abort"
+      exit 2
+   fi
+}
+
+install_centos() {
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.27/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.26/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.25/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.24/rpm/
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+  
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+}
+
+install_amzn() {
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/repodata/repomd.xml.key
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.29/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.27/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.26/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.25/rpm/
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.24/rpm/
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+  
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+}
+################ MAIN ###################
+
+if [ -f /etc/os-release ];then
+   osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT|PLATFORM' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
+   echo $osname
+   if [ $osname == "ubuntu" ];then
+       install_ubuntu
+   elif [ $osname == "amzn" ];then
+       install_amzn
+   elif [ $osname == "centos" ];then
+       install_centos
+  fi
+else
+   echo "can not locate /etc/os-release - unable find the osname"
+   exit 8
+fi
+exit 0
